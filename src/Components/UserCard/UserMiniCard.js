2d305f8f420f4b192419b8597e3760478db7f92e
aakashjakhmola <aakash.jakhmola@codingninjas.com>
2021-12-05 14:32:26 +0530

    fixed bugs & changed api_url

--- src/Components/UserCard/UserMiniCard.js
+++ src/Components/UserCard/UserMiniCard.js
@@ -1,17 +1,49 @@
-import React from 'react'
-import './UserMiniCard.scss'
+import React, { useEffect, useState } from 'react';
+import './UserMiniCard.scss';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
+import ProfilePic from '../../assets/profile_icon.png';
+import { API_URL } from '../../utils/Constants';
 
-import ProfilePic from '../../assets/profile_icon.png'
+export default function UserMiniCard({ user_details, is_following }) {
+  // console.log(props)
 
-export default function UserMiniCard(props) {
- // console.log(props)
+
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    if (is_following) {
+      setIsFollowing(true);
+    }
+  })
+
+  const followUser = () => {
+    axios.post(`${API_URL}/api/v2/profile/follow`, { following_username: user_details.username }, { withCredentials: true })
+      .then(() => { setIsFollowing(true) })
+      .catch((err) => { console.log(err) })
+  }
+
+  const unfollowUser = () => {
+
+    axios.delete(`${API_URL}/api/v2/profile/unfollow?following_username=${user_details.username}`, { withCredentials: true })
+      .then(() => { setIsFollowing(false) })
+      .catch((err) => { console.log(err) })
+  }
+
+
   return (
     <div className='user-mini-card'>
-      <div className='profile-img'> <img src={ProfilePic} style={{height:'3rem'}}/></div>
+      <div className='profile-img'> <img src={ProfilePic} style={{ height: '3rem' }} /></div>
       <div className='user-info'>
-        <span style={{fontSize:'1.2em'}}>{props.firstname} {props.lastname}</span>
-      <br/>
-      <span style={{fontWeight:'300'}}>@{props.username}</span>
+        <Link to={`/${user_details?.username}`}>
+          <span style={{ fontSize: '1.2em' }}>{user_details?.firstname} {user_details?.lastname}</span>
+          <br />
+          <span style={{ fontWeight: '300' }}>@{user_details?.username}</span>
+        </Link>
+      </div>
+      <div className="umc-buttons">
+        {isFollowing && <button className={'umc-btn unfollow-btn'} onClick={unfollowUser}>Unfollow</button>}
+        {!isFollowing && <button className={'umc-btn follow-btn'} onClick={followUser}>Follow</button>}
       </div>
     </div>
   )
