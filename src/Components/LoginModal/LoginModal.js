f5ee6ab62f27993c65512464c639be527c9ff545
Aakash Jakhmola <aakash001jakhmola@gmail.com>
2022-03-19 12:39:38 +0530

    ui fixes

--- src/Components/LoginModal/LoginModal.js
+++ src/Components/LoginModal/LoginModal.js
@@ -1,74 +1,92 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
 import { Redirect } from 'react-router';
 
-import './LoginModal.scss'
+import './LoginModal.scss';
 import { FormGroup, Container, Row, Col } from 'reactstrap';
 import Loader from '../Loader/Loader';
 import { loginUser } from '../../state/auth/authActions';
 import { AuthContext } from '../../state/Store';
 
 function LoginModal(props) {
-  const auth = useContext(AuthContext)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const auth = useContext(AuthContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const loginSubmit = () => {
-    console.log(username, password)
-    let userCred = { username: username, password: password }
-    loginUser(auth.dispatch, userCred)
-  }
-
+    console.log(username, password);
+    let userCred = { username: username, password: password };
+    loginUser(auth.dispatch, userCred);
+  };
 
   if (auth.state.userLoggedIn) {
-    return <Redirect
-      to={(props.location.state && props.location.state.from) || "/"}
-    />
+    return (
+      <Redirect
+        to={(props.location.state && props.location.state.from) || '/'}
+      />
+    );
   }
 
   return (
     <div className='login-modal-overlay'>
       <div className='login-modal'>
-        {props.setLoginOpen && <i className="fas fa-times modal-close-btn" onClick={() => { props.setLoginOpen(false) }} />}
+        {props.setLoginOpen && (
+          <i
+            className='fas fa-times modal-close-btn'
+            onClick={() => {
+              props.setLoginOpen(false);
+            }}
+          />
+        )}
         <Container>
           <Row>
-            <Col><h2>Login</h2></Col>
+            <Col>
+              <h2>Login</h2>
+            </Col>
           </Row>
           <Row>
-            <Col md="12">
+            <Col md='12'>
               <FormGroup>
-                <label htmlFor="username" className="fontType">
+                <label htmlFor='username' className='fontType'>
                   USERNAME
                 </label>
                 <input
-                  className="form-control"
-                  type="text"
+                  className='form-control'
+                  type='text'
                   id='username'
                   value={username}
-                  onChange={(e) => { setUsername(e.target.value) }}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
                 />
               </FormGroup>
             </Col>
           </Row>
           <Row>
-            <Col md="12">
+            <Col md='12'>
               <FormGroup>
-                <label htmlFor="password" className="fontType">
+                <label htmlFor='password' className='fontType'>
                   PASSWORD
                 </label>
                 <input
-                  className="form-control"
-                  type="password"
-                  id="password"
+                  className='form-control'
+                  type='password'
+                  id='password'
                   value={password}
-                  onChange={(e) => { setPassword(e.target.value) }}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                 />
               </FormGroup>
             </Col>
           </Row>
           <Row>
-            <Col md="12">
+            <Col md='12'>
               <FormGroup>
-                {auth.state.userLoggingIn ? <Loader /> : <button onClick={loginSubmit}> Login </button>}
+                {auth.state.userLoggingIn ? (
+                  <Loader />
+                ) : (
+                  <button onClick={loginSubmit}> Login </button>
+                )}
               </FormGroup>
             </Col>
           </Row>
@@ -78,7 +96,7 @@ function LoginModal(props) {
         </Container>
       </div>
     </div>
-  )
+  );
 }
 
-export default LoginModal
+export default LoginModal;
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,154 +1,186 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
-import './SignUp.scss'
-import { FormGroup,Container, Row, Col } from 'reactstrap';
-import { API_URL } from '../../utils/Constants'
+import './SignUp.scss';
+import backButton from './../../assets/icons/back-button.svg';
+import { FormGroup, Container, Row, Col } from 'reactstrap';
+import { API_URL } from '../../utils/Constants';
 
 import Loader from '../../Components/Loader/Loader';
+import { useHistory } from 'react-router-dom';
 
 export default function SignUp() {
+  const history = useHistory();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confPass, setConfPass] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [confPass, setConfPass] = useState('')
-  const [errorMsg, setErrorMsg] = useState('')
-  const [loading, setLoading] = useState(false)
+  function goBack() {
+    history.goBack();
+  }
 
-  function signup () {
-    if(!firstName) {
-      setErrorMsg('First Name cannot be empty')
-      return
-    }else if(!username) {
-      setErrorMsg('Username cannot be empty')
-      return
-    }else if(!password) {
-      setErrorMsg('Password cannot be empty')
-      return
+  function signup() {
+    if (!firstName) {
+      setErrorMsg('First Name cannot be empty');
+      return;
+    } else if (!username) {
+      setErrorMsg('Username cannot be empty');
+      return;
+    } else if (!password) {
+      setErrorMsg('Password cannot be empty');
+      return;
+    } else if (password !== confPass) {
+      setErrorMsg('Passwords do not match');
+      return;
     }
-    else if(password!==confPass) {
-      setErrorMsg('Passwords do not match')
-      return
-    }
-    setErrorMsg("")
-    let user = {firstname:firstName,
-                lastname:lastName,
-                username:username,
-                password:password}
-    console.log(user)
-    setLoading(true)
-    axios.post(`${API_URL}/users/register`,user)
-      .then((res)=>{
-        console.log(res.data)
-        setLoading(false)
+    setErrorMsg('');
+    let user = {
+      firstname: firstName,
+      lastname: lastName,
+      username: username,
+      password: password,
+    };
+    console.log(user);
+    setLoading(true);
+    axios
+      .post(`${API_URL}/users/register`, user)
+      .then((res) => {
+        console.log(res.data);
+        setLoading(false);
       })
-      .catch((error)=> {
-        console.log('printing',error.response.data)
-        if(error.response && error.response.data.error) {
-          setErrorMsg(error.response.data.error)
+      .catch((error) => {
+        console.log('printing', error.response.data);
+        if (error.response && error.response.data.error) {
+          setErrorMsg(error.response.data.error);
         } else {
-          setErrorMsg('something went wrong')
+          setErrorMsg('something went wrong');
         }
-        setLoading(false)
-      })
+        setLoading(false);
+      });
   }
 
   return (
     <div className='signup'>
       <div className='signup-left'>
-       <div className='signup-box'>
-        <Container>
-          <Row>
-            <Col><h2>Sign up</h2></Col>
-          </Row>
-          <Row>
-            <Col md="6">
-              <FormGroup>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="First Name*"
-                  value={firstName}
-                  onChange={(e)=>{setFirstName(e.target.value)}}
-                />
-              </FormGroup>
-            </Col>
-            <Col md="6">
-              <FormGroup>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="Last Name"
-                  value={lastName}
-                  onChange={(e)=>{setLastName(e.target.value)}}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="12">
-              <FormGroup>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="Username*"
-                  value={username}
-                  onChange={(e)=>{setUsername(e.target.value)}}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="12">
-              <FormGroup>
-                <input
-                  className="form-control"
-                  type="password"
-                  placeholder="Password*"
-                  value={password}
-                  onChange={(e)=>{setPassword(e.target.value)}}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="12">
-              <FormGroup>
-                <input
-                  className="form-control"
-                  type="password"
-                  placeholder="Confirm Password*"
-                  value={confPass}
-                  onChange={(e)=>{setConfPass(e.target.value)}}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-              <Col md="12">
+        <img className='back-btn' src={backButton} onClick={goBack} />
+        <div className='signup-box'>
+          <Container>
+            <Row>
+              <Col>
+                <h2>Create a New Account</h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col md='6'>
                 <FormGroup>
-                {loading? <Loader/> :<button onClick={signup}> Sign Up </button>}
+                  <input
+                    className='form-control'
+                    type='text'
+                    placeholder='First Name*'
+                    value={firstName}
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md='6'>
+                <FormGroup>
+                  <input
+                    className='form-control'
+                    type='text'
+                    placeholder='Last Name'
+                    value={lastName}
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
+                  />
                 </FormGroup>
               </Col>
             </Row>
             <Row>
-              <Col md="12">
-                <div style={{color:'red'}}>{errorMsg?errorMsg:""}</div>
+              <Col md='12'>
+                <FormGroup>
+                  <input
+                    className='form-control'
+                    type='text'
+                    placeholder='Username*'
+                    value={username}
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                    }}
+                  />
+                </FormGroup>
               </Col>
             </Row>
-        </Container>
-        
-       </div>
+            <Row>
+              <Col md='12'>
+                <FormGroup>
+                  <input
+                    className='form-control'
+                    type='password'
+                    placeholder='Password*'
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md='12'>
+                <FormGroup>
+                  <input
+                    className='form-control'
+                    type='password'
+                    placeholder='Confirm Password*'
+                    value={confPass}
+                    onChange={(e) => {
+                      setConfPass(e.target.value);
+                    }}
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md='12'>
+                <FormGroup>
+                  {loading ? (
+                    <Loader />
+                  ) : (
+                    <button onClick={signup}> Sign Up </button>
+                  )}
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row className='login-link'>
+              <a href='/login'> Already Have an Account ? </a>
+            </Row>
+            <Row>
+              <Col md='12'>
+                <div style={{ color: 'red' }}>{errorMsg ? errorMsg : ''}</div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
 
-     <div className='signup-right'>
-       <div className='right-box-content'>
-        <p style={{fontSize:'2.7rem', fontWeight:'700', lineHeight:1}}> Explore New Movies</p>
-        <p>Find the best movies picked by your friends. Share the movies you loved. Help your friends.</p>
-       </div>
-     </div>
-
+      <div className='signup-right'>
+        <div className='right-box-content'>
+          <p style={{ fontSize: '2.7rem', fontWeight: '700', lineHeight: 1 }}>
+            {' '}
+            Explore New Movies
+          </p>
+          <p>
+            Find the best movies picked by your friends. Share the movies you
+            loved. Help your friends.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
