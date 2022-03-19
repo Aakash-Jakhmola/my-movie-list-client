e3adc84c7d0fc06d024032df49e27383ccaec1f3
Aakash Jakhmola <aakash001jakhmola@gmail.com>
2022-03-19 13:05:54 +0530

    fix api endpoints

--- src/pages/Account/Following.js
+++ src/pages/Account/Following.js
@@ -1,41 +1,48 @@
-import React,{useEffect,useState} from 'react'
-import axios from 'axios'
-import {API_URL} from '../../utils/Constants'
-import { useParams } from 'react-router'
-import UserMiniCard from '../../Components/UserCard/UserMiniCard'
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../utils/Constants';
+import { useParams } from 'react-router';
+import UserMiniCard from '../../Components/UserCard/UserMiniCard';
 
 const EmptyFollowingList = () => {
-  return <div style={{textAlign:'center'}}>
-    You don't follow anyone
-    Follow your friends to see what movies they are watching.
-  </div>;
-}
+  return (
+    <div style={{ textAlign: 'center' }}>
+      You don't follow anyone Follow your friends to see what movies they are
+      watching.
+    </div>
+  );
+};
 
 export default function Following() {
+  const { username } = useParams();
 
-  const {username} = useParams()
-  
-  const [following, setFollowing] = useState([])
+  const [following, setFollowing] = useState([]);
 
-  useEffect(()=>{
-    axios.get(`${API_URL}/api/v2/profile/following?username=${username}`)
-    .then((res)=>{
-      console.log(res.data)
-      setFollowing(res.data)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  },[])
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/account/${username}/following`)
+      .then((res) => {
+        console.log(res.data);
+        setFollowing(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
-    <div style={{margin:'4rem 15rem'}}>
-      {following && following.length > 0 ? following.map((f)=>(<UserMiniCard 
-      user_details={f.following_details} 
-      is_following = {true}
-      fromFollowingList={true}
-      />)) : <EmptyFollowingList />}
+    <div style={{ margin: '4rem 15rem' }}>
+      {following && following.length > 0 ? (
+        following.map((f) => (
+          <UserMiniCard
+            user_details={f}
+            is_following={true}
+            fromFollowingList={true}
+          />
+        ))
+      ) : (
+        <EmptyFollowingList />
+      )}
     </div>
-  )
+  );
 }
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,68 +1,69 @@
-import React, { useState, useContext, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
 
-import './Feed.scss'
-import ProfilePic from '../../assets/profile_icon.png'
-import { API_URL } from '../../utils/Constants'
-import NavBar from '../../Components/Navbar/Navbar'
-import { AuthContext } from '../../state/Store'
-import FeedCard from '../../Components/FeedCard/FeedCard'
-import { NavLink } from 'react-router-dom'
-import MovieSearchCard from '../../Components/MovieSearchCard/MovieSearchCard'
+import './Feed.scss';
+import ProfilePic from '../../assets/profile_icon.png';
+import { API_URL } from '../../utils/Constants';
+import NavBar from '../../Components/Navbar/Navbar';
+import { AuthContext } from '../../state/Store';
+import FeedCard from '../../Components/FeedCard/FeedCard';
+import { NavLink } from 'react-router-dom';
+import MovieSearchCard from '../../Components/MovieSearchCard/MovieSearchCard';
 
 function Feed(props) {
-  const auth = useContext(AuthContext)
+  const auth = useContext(AuthContext);
 
   const inituser = {
     username: auth.state.username,
-    firstname: "",
-    lastname: "",
+    firstname: '',
+    lastname: '',
     movies_count: 0,
     followers_count: 0,
     following_count: 0,
     watch_later_count: 0,
-  }
+  };
 
-  const [user, setUser] = useState(inituser)
-  const [feeds, setFeeds] = useState([])
-  const [trendingMovies, setTrendingMovies] = useState([])
-  const [ratingModalOpen, setRatingModalOpen] = useState(false)
+  const [user, setUser] = useState(inituser);
+  const [feeds, setFeeds] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [ratingModalOpen, setRatingModalOpen] = useState(false);
 
-  
   useState(() => {
     console.log(auth);
-    props.setLoginOpen(false)
-    axios.get(`${API_URL}/api/v2/users/feed`, { withCredentials: true })
+    props.setLoginOpen(false);
+    axios
+      .get(`${API_URL}/account/feed`, { withCredentials: true })
       .then((res) => {
-        setFeeds(res.data)
+        setFeeds(res.data);
       })
       .catch((err) => {
-        console.log(err.response)
-      })
-  }, [])
+        console.log(err.response);
+      });
+  }, []);
 
   useEffect(() => {
-    axios.get(`${API_URL}/account?username=${auth.state.username}`)
+    axios
+      .get(`${API_URL}/account?username=${auth.state.username}`)
       .then((res) => {
         setUser(res.data[0]);
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [auth.state])
+        console.log(err);
+      });
+  }, [auth.state]);
 
   useEffect(() => {
-    axios.get(`${API_URL}/movie/trending`)
+    axios
+      .get(`${API_URL}/movie/trending`)
       .then((res) => {
-        console.log('trending', res.data)
+        console.log('trending', res.data);
         setTrendingMovies(res.data);
-        
       })
       .catch((err) => {
-        console.log(err.response)
-      })
-  }, [])
+        console.log(err.response);
+      });
+  }, []);
 
   return (
     <>
@@ -70,28 +71,31 @@ function Feed(props) {
       <div className='feed-wrapper'>
         <div className='trending-movies'>
           Trending Movies
-
-          {trendingMovies && trendingMovies.length > 0 && trendingMovies.map((m) => (
-            <MovieSearchCard
-              movie={m}
-              ratingModalOpen={ratingModalOpen}
-              setRatingModalOpen={setRatingModalOpen}
-             />)
-          )}
-
+          {trendingMovies &&
+            trendingMovies.length > 0 &&
+            trendingMovies.map((m) => (
+              <MovieSearchCard
+                movie={m}
+                ratingModalOpen={ratingModalOpen}
+                setRatingModalOpen={setRatingModalOpen}
+              />
+            ))}
         </div>
 
         <div className='feed'>
-          {feeds && feeds.length > 0 && feeds.map((f) => (
-            <FeedCard {...f} />
-          ))}
+          {feeds && feeds.length > 0 && feeds.map((f) => <FeedCard {...f} />)}
         </div>
 
         <div className='feed-details'>
           <div className='user-card'>
-            <div className='profile-img'> <img src={ProfilePic} style={{ height: '3rem' }} /></div>
+            <div className='profile-img'>
+              {' '}
+              <img src={ProfilePic} style={{ height: '3rem' }} />
+            </div>
             <div className='user-info'>
-              <span style={{ fontSize: '1.2em' }}>{user.firstname} {user.lastname}</span>
+              <span style={{ fontSize: '1.2em' }}>
+                {user.firstname} {user.lastname}
+              </span>
               <br />
               <span style={{ fontWeight: '300' }}>@{user.username}</span>
             </div>
@@ -99,23 +103,30 @@ function Feed(props) {
 
           <div className='user-options'>
             <div className='option'>
-              <NavLink to={`/${user.username}`} className="link"><i className="fas fa-film" /> Movies</NavLink>
+              <NavLink to={`/${user.username}`} className='link'>
+                <i className='fas fa-film' /> Movies
+              </NavLink>
             </div>
             <div className='option'>
-              <NavLink to={`/${user.username}/watchlist`} className="link"><i className="far fa-clock" /> Watchlist</NavLink>
+              <NavLink to={`/${user.username}/watchlist`} className='link'>
+                <i className='far fa-clock' /> Watchlist
+              </NavLink>
             </div>
             <div className='option'>
-              <NavLink to={`/${user.username}/followers`} className="link"><i className="fas fa-users" /> Followers</NavLink>
+              <NavLink to={`/${user.username}/followers`} className='link'>
+                <i className='fas fa-users' /> Followers
+              </NavLink>
             </div>
             <div className='option'>
-              <NavLink to={`/${user.username}/following`} className="link"><i className="fas fa-user-friends" /> Following</NavLink>
+              <NavLink to={`/${user.username}/following`} className='link'>
+                <i className='fas fa-user-friends' /> Following
+              </NavLink>
             </div>
           </div>
-
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
