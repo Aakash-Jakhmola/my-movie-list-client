39402c0deb3dd6b9b3a8becae4446e5c62c83931
Aakash-Jakhmola <aakash001jakhmola@gmail.com>
2022-05-23 19:02:34 +0530

    update url

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:8000"
-export const API_URL = "https://guarded-retreat-89351.herokuapp.com/"
+export const API_URL = "https://guarded-retreat-89351.herokuapp.com"
 
